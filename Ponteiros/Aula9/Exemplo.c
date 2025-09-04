980b7a9804b482188e555ffa14fa38cb721f2f06
Leandro <lsavio516@gmail.com>
2025-09-04 18:15:26 -0300

    Aula 9

---
+++ Ponteiros/Aula9/Exemplo.c
@@ -0,0 +1,24 @@
+// Incremento de Ponteiros
+#include <stdio.h>
+
+int main(){
+    int n;
+    int *pn = &n;
+    printf("Informe a quantidade de valores: \n");
+    scanf("%d", pn);
+    int v[n];
+    int i;
+    for (i = 0; i < n; i++){
+        scanf("%d", &v[i]);
+    }
+    int imenor = 0;
+    int *pv = v + 1;
+    for (i = 1; i < n; i++){
+        if (*pv < v[imenor]){
+            imenor = i;
+        }
+        pv++;
+    }
+    printf("MENOR: %d\n", imenor);
+    return 0;
+}
