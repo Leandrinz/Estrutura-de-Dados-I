6d278a40ad7865812e33379885b1d406455022f5
Leandro <lsavio516@gmail.com>
2025-11-18 09:14:41 -0300

    Aula 48 - Lendo  vetor de um arquivo

---
+++ Arquivo_texto/Aula48/Exemplo.c
@@ -0,0 +1,23 @@
+// Lendo um vetor de um arquivo
+#include <stdio.h>
+
+typedef struct 
+{
+    char nome[50];
+    float nota1;
+    float nota2;
+    float nota3;
+}estudante;
+
+int main(){
+    FILE *file = fopen("../estudantes.b", "rb");
+    estudante v[3];
+    fread(v, sizeof(estudante), 3, file);
+    int i;
+    for (i = 0; i < 3; i++){
+        float media = (v[i].nota1 + v[i].nota2 + v[i].nota3) / 3;
+        printf("%s:%.2f\n", v[i].nome, media);
+    }
+    fclose(file);
+    return 0;
+}
