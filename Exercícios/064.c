99e9e3ff63c9a9371b6f134df0ab5bb182333aea
Leandro <lsavio516@gmail.com>
2025-11-21 17:30:12 -0300

    Exercício 64

---
+++ Exercícios/064.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Digite o valor de N:\n");
+    scanf("%d", &n);
+    int v[n];
+    int i, j;
+    for (i = 0; i < n; i++){
+        scanf("%d", &v[i]);
+    }
+
+    for (i = 1; i < n; i++){
+        int x = v[i];
+        for (j = i - 1; j >= 0 && x < v[j]; j--){
+            v[j + 1] = v[j];
+        }
+        v[j + 1] = x;
+    }
+    printf("Imprimindo o vetor ordenado:\n");
+    for (i = 0; i < n; i++){
+        printf("%d ", v[i]);
+    }
+    return 0;
+}
