c5c4dc6ac1794fbdd9eea145baac634ba619065c
Leandro <lsavio516@gmail.com>
2025-11-27 19:43:18 -0300

    Aula 73 e 74 - Algoritmos de Partição

---
+++ Algoritmos_de_Partição/Aula73/Exemplo.c
@@ -0,0 +1,40 @@
+// Vetor secundário
+#include <stdio.h>
+
+void particao(int n, int v[]){
+    int s[n];
+    int e = 0;
+    int d = n - 1;
+    int pivo = v[n - 1];
+    int i;
+    for (i = 0; i < n; i++){
+        if (v[i] < pivo){
+            s[e] = v[i];
+            e++;
+        }
+        else{
+            s[d] = v[i];
+            d--;
+        }
+    }
+    for (i = 0; i < n; i++){
+        v[i] = s[i];
+    }
+}
+
+int main(){
+    int n;
+    printf("Informe o valor de N:\n");
+    scanf("%d", &n);
+    int v[n];
+    printf("Imprimindo o vetor:\n");
+    int i;
+    for (i = 0; i < n; i++){
+        scanf("%d", &v[i]);
+    }
+    particao(n, v);
+    for (i = 0; i < n; i++){
+        printf("%d ", v[i]);
+    }
+    return 0;
+}
