78425cbc1148344dc614e354f068e598e5d18e06
Leandro <lsavio516@gmail.com>
2025-12-01 15:23:47 -0300

    Aula 85 - Inserindo apos um dado em fila

---
+++ Lista/Aula85/Exemplo.c
@@ -0,0 +1,130 @@
+// Inserindo um item após um dado nó em uma lista encadeada
+// Inserindo um item no fim de uma lista encadeada
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int mat;
+    char nome[50];
+}estudante;
+
+struct No{
+    estudante dado;
+    struct No * prox;
+};
+
+typedef struct {
+    struct No *inicio;
+}Lista;
+
+void menu(){
+    printf("=== Escolha uma opcao ===\n");
+    printf("1 - Inserir inicio\n");
+    printf("2 - Inserir no fim\n");
+    printf("3 - Inserir apos\n");
+    printf("4 - Mostrar lista\n");
+    printf("5 - Pesquisar por matricula\n");
+    printf("6 - Sair\n");
+    printf("=========================\n");
+}
+
+void ler(estudante *pe){
+    scanf("%d %s", &pe->mat, pe->nome);
+}
+
+void inserir_inicio(Lista *plista, estudante dado){
+    struct No *novo = (struct No*) malloc(sizeof(struct No));
+    novo->dado = dado;
+    novo->prox = plista->inicio;
+    plista->inicio = novo;
+}
+
+void inserir_fim(Lista *plista, estudante dado){
+    struct No *novo = (struct No*) malloc(sizeof(struct No));
+    novo->dado = dado;
+    novo->prox = NULL;
+    if (plista->inicio == NULL){
+        plista->inicio = novo;
+    }
+    else{
+        struct No *pi;
+        for (pi = plista->inicio; pi->prox != NULL; pi = pi->prox);
+        pi->prox = novo;
+    }
+}
+
+struct No* inserir_apos(Lista *plista, estudante dado, int mat){
+    struct No * pi;
+    for (pi = plista->inicio; pi != NULL && pi->dado.mat != mat; pi = pi->prox);
+    if (pi == NULL){
+        return pi;
+    }
+    else{
+        struct No *novo = (struct No*) malloc(sizeof(struct No));
+        novo->prox = pi->prox;
+        pi->prox = novo;
+        return pi;
+    }
+}
+
+void mostrar(Lista lista){
+    struct No * pi = lista.inicio;
+    for (pi = lista.inicio; pi != NULL; pi = pi->prox){
+        printf("%d %s\n", pi->dado.mat, pi->dado.nome);
+    }
+}
+
+struct No* pesquiar (Lista lista, int mat){
+    struct No * pi;
+    for (pi = lista.inicio; pi != NULL && pi->dado.mat != mat; pi = pi->prox);
+    return pi;
+}
+
+int main(){
+    Lista lista;
+    lista.inicio = NULL;
+    int op;
+    menu();
+    scanf("%d", &op);
+    while (op != 6){
+        if (op == 1){
+            estudante e;
+            ler(&e);
+            inserir_inicio(&lista, e);
+        }
+        if (op == 2){
+            estudante e;
+            ler(&e);
+            inserir_fim(&lista, e);
+        }
+        if (op == 3){
+            estudante e;
+            ler (&e);
+            int mat;
+            printf("Informe a matricula para inserir apos:\n");
+            scanf("%d", &mat);
+            struct No * pi = inserir_apos(&lista, e, mat);
+            if (pi == NULL){
+                printf("Não foi possivel inserir o estudante\n");
+            }
+        }
+        if (op == 4){
+            mostrar(lista);
+        }
+        if (op == 5){
+            int mat;
+            printf("Informe a matricula\n");
+            scanf("%d", &mat);
+            struct No * pi = pesquisar(lista, mat);
+            if (pi != NULL){
+                printf("%d %s\n", pi->dado.mat, pi->dado.nome);
+            }
+            else{
+                printf("Estudante nao encontrado\n");
+            }
+        }
+        menu();
+        scanf("%d", &op);
+    }
+    return 0;
+}
