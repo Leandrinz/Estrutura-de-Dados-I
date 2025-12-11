15287f11b052e3de19e16b55b009b4cfae42fde0
Leandro <lsavio516@gmail.com>
2025-12-11 16:54:03 -0300

    Aula 102 - Removendo um elemento da pilha

---
+++ Pilha/Aula102/Exemplo.c
@@ -0,0 +1,83 @@
+// Apagando um elemento da pilha
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int mat;
+    char nome[50];
+}estudante;
+
+struct no{
+    estudante dado;
+    struct no * prox;
+};
+
+typedef struct no No;
+
+typedef struct {
+    No * topo;
+}Pilha;
+
+void menu(){
+    printf("==== escolha uma opcao ====\n");
+    printf("1 - adicionar\n");
+    printf("2 - Mostrar pilha\n");
+    printf("3 - Remover\n");
+    printf("4 - Sair\n");
+    printf("===========================\n");
+}
+
+void ler(estudante *pe){
+    scanf("%d %s", &pe->mat, pe->nome);
+}
+//push
+void inserir(Pilha *ppilha, estudante dado){
+    No * novo = (No*) malloc(sizeof(No));
+    novo->dado = dado;
+    novo->prox = ppilha->topo;
+    ppilha->topo = novo;
+}
+// pop
+void remover(Pilha *ppilha){
+    if (ppilha->topo == NULL){
+        printf("A pilha esta vazia\n");
+    }
+    else{
+        No * pi = ppilha->topo;
+        ppilha->topo = pi->prox;
+        free(pi);
+    }
+}
+
+
+void mostrar_pilha(Pilha pilha){
+    No * pi;
+    for (pi = pilha.topo; pi != NULL; pi = pi->prox){
+        printf("%d %s\n", pi->dado.mat, pi->dado.nome);
+    }
+}
+
+int main(){
+    Pilha pilha;
+    pilha.topo = NULL;
+    int op;
+    menu();
+    scanf("%d", &op);
+    while (op != 4){
+        if (op == 1){
+            estudante e;
+            printf("Informe os dados do estudante\n");
+            ler(&e);
+            inserir(&pilha, e);
+        }
+        else if (op == 2){
+            mostrar_pilha(pilha);
+        }
+        else if (op == 3){
+            remover(&pilha);
+        }
+        menu();
+        scanf("%d", &op);
+    }
+    return 0;   
+}
